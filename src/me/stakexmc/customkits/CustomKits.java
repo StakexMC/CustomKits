cf3ec03d43c918adeabe9989ee374ffed05117e5
Pedro Martinez <pedrojm1996@gmail.com>
2017-04-01 17:42:46 -0430

    Se agrego un mensaje en el enable

--- src/me/stakexmc/customkits/CustomKits.java
+++ src/me/stakexmc/customkits/CustomKits.java
@@ -4,7 +4,12 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class CustomKits extends JavaPlugin {
 	
+	private CoreLog log;
+	
 	public void onEnable(){
-		
+		this.log = new CoreLog(this);
+		log.line("--------------------------------");
+		log.info("Hola bebe");
+		log.line("--------------------------------");
 	}
 }
