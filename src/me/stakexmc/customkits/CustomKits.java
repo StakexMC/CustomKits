901f9b3d5a9fac6d87f124de9b008e1eee0d0209
StakexMC <Stakexkun@gmail.com>
2017-04-03 15:47:57 -0400

    Añadi archivo de kits

--- src/me/stakexmc/customkits/CustomKits.java
+++ src/me/stakexmc/customkits/CustomKits.java
@@ -14,6 +14,7 @@ public class CustomKits extends JavaPlugin {
 		log.info("Hola bebe");
 		log.info("Pruebaaaaaaaaaaaa");
 		iniConfig();
+		iniKits();
 		log.line("--------------------------------");
 	}
 	   
@@ -25,6 +26,16 @@ public class CustomKits extends JavaPlugin {
 				cconfig.load();
 			}else{
 				configDefault();
+				cconfig.create();
+			}
+			
+		}public void iniKits(){
+			cconfig = new CoreConfig(intance,"Kits");
+			if (cconfig.Exists()){
+				configKits();
+				cconfig.load();
+			}else{
+				configKits();
 				cconfig.create();
 			}
 			
@@ -41,6 +52,16 @@ public class CustomKits extends JavaPlugin {
 			cconfig.add("dataStorage.database","minecraft");
 			cconfig.add("dataStorage.username","root");
 			cconfig.add("dataStorage.password","1234");
+		}
+		//Configuracion default para el archivo Kits
+		public void configKits(){	
+			cconfig.add("Survival.Name","&7Survival");
+			cconfig.add("Survival.Icon","299");
+			cconfig.add("Survival.helmet","298");
+			cconfig.add("Survival.chesplate","299");
+			cconfig.add("Survival.leggings","300");
+			cconfig.add("Survival.boots","301");
+			
 		}
 	
 	public CoreLog getLog(){
